0f089026a5613f10fcd8d2edb585d70cbdf5eb3d
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-08 16:50:20 -0700

    code refactor in index.js and MVP

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const session = require("express-session");
 const KnexSessionStore = require("connect-session-knex")(session);
 const db = require("./database/config");
-//const usersRouter = require("./users/users-router")
+const authUsersRouter = require("./authUsers/authUsers-router");
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -24,7 +24,7 @@ server.use(
   })
 );
 
-//server.use(usersRouter)
+server.use(authUsersRouter);
 
 server.use((err, req, res, next) => {
   console.log(err);
